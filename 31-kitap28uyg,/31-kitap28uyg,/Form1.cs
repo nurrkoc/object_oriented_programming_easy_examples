3b431bfbfd4de44186f5084af020cabd1fbde91b
Nur Koç <150712549+nurrkoc@users.noreply.github.com>
2023-12-01 21:13:59 +0300

    Add files via upload

---
+++ 31-kitap28uyg,/31-kitap28uyg,/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _31_kitap28uyg_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 0,toplam=0;
+            while (toplam<1000)
+            {
+                listBox1.Items.Add(sayac);
+                toplam += sayac;
+                sayac++;                
+            }
+            MessageBox.Show("Sayac : "+sayac.ToString());
+            MessageBox.Show("Tplam : " + toplam.ToString());
+        }
+    }
+}
